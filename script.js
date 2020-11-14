1e34436cef8d7c26dd7735090a6afb12af1e9417
Paul Carr <pwcarr15@gmail.com>
2020-11-13 23:22:25 -0500

    Added section to js for api key, array, and click event listener plus alert for empty field.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 
-    
+    var APIKey = "&units=metric&appid=e222da09846714641d1de2e70cf4f020";
+  
+    var searchArray = [];
+
+    $("#search-button").on("click", function() {
+        event.preventDefault()
+        if($("#city-name").val() === "") {
+            alert("You must type in a city to look up.");
+            return;
+        }
+        
+        
+    })
 })
