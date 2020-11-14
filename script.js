72aacc4b88e935ffacddb4e48a8c83f5a05fd4b9
Paul Carr <pwcarr15@gmail.com>
2020-11-13 23:23:17 -0500

    Added section to pull data from api to get current temp in city searched.

--- script.js
+++ script.js
@@ -10,7 +10,36 @@ $(document).ready(function() {
             alert("You must type in a city to look up.");
             return;
         }
+        var cityName = $("#city-name").val();
+        searchArray.push(cityName);
+        $("#city-name").val("");
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIKey;
         
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var city = response.name;
+            $("#city").text(city);
+            var temp = response.main.temp;
+            $("#temp").text("Temp: " + temp);
+            var humid = response.main.humidity;
+            $("#humid").text("Humidity: " + humid);
+            var wind = response.wind.speed;
+            $("#wind").text("Wind Speed: " + wind);
+            var iconDiv = $("<img>");
+            var icon = response.weather[0].icon;
+            var iconSrc = "https://openweathermap.org/img/w/" + icon + ".png";
+            iconDiv.attr("src", iconSrc);
+            $("#icon").html(iconDiv);
+
+            renderHistory();
+  
+        })
+
+        
+
         
     })
 })
